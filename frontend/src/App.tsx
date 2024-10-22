460eaf5aabf83a0a510a3cf4531e41b3e89611df
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-22 01:14:49 -0300

    removing unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Student from "./Components/Student/Student";
 import CreateStudent from "./Components/Student/CreateStudent";
