341c0add38ecdf6f52ddd138f93243fbd6e3ba38
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-22 00:32:28 -0300

    connecting to database and fetching data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const mysql = require("mysql");
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "react_crud"
+});
+
+app.get("/healthcheck", (req, res) => {
+    res.status(200);
+    res.json({msg: "OK"});
+});
+
+app.get("/", (req, res) => {
+    const query = "SELECT * FROM student";
+
+    db.query(query, (err, data) => {
+        if (err) return res.json({msg: err.message});
+        else return res.json(data);
+    });
+});
+
 app.listen(8081, () => {
     console.log("listening");
 });
